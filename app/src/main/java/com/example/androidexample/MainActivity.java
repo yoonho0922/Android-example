39cf86a0e091fc843ffa0cf32e5c8f6b84291bf7
201910812 <201910812@yoonho0922>
2020-04-12 20:56:11 +0900

    example1 update

--- app/src/main/java/com/example/androidexample/MainActivity.java
+++ app/src/main/java/com/example/androidexample/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.androidexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button bt1;
+    Button bt2;
+    Button bt3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bt1 = findViewById(R.id.button1);
+        bt2 = findViewById(R.id.button2);
+        bt3 = findViewById(R.id.button3);
+
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Example1.class);
+                startActivity(intent);
+            }
+        });
     }
 }
