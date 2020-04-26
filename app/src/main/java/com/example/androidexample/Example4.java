08666114dc6d412908e38d114a5c730192abb2c4
201910812 <201910812@yoonho0922>
2020-04-26 16:14:47 +0900

    깃 변경 확인

---
+++ app/src/main/java/com/example/androidexample/Example4.java
@@ -0,0 +1,14 @@
+package com.example.androidexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Example4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_example4);
+    }
+}
